a673247c7ebe8902dd493cd8e73af54f51ee97f9
Andrew Wong <andrew12345@live.com>
2014-10-18 18:35:37 +1100

    (Add cross browser support)
    I forgot to sync my commits :D
    Or I forgot to commit.

--- userscripts/csgolounge_available-items-formatter.user.js
+++ userscripts/csgolounge_available-items-formatter.user.js
@@ -4,12 +4,10 @@
 // @author      bearbear12345
 // @namespace   http://csgolounge.com/availableweapons
 // @include     http://csgolounge.com/availableweapons
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 document.title="Available items in CSGO Lounge";
-var iStyle = document.createElement('style');  
-document.getElementsByTagName('body')[0].appendChild(iStyle);
-var style = document.styleSheets[0];
-style.insertRule(".hover_bold:hover {font-weight: bold;}",document.styleSheets[0].cssRules.length);
+document.head.appendChild(document.createElement('style'));
+document.getElementsByTagName('style')[0].sheet.insertRule(".hover_bold:hover {font-weight: bold;}",document.getElementsByTagName('style')[0].sheet.cssRules.length);
 document.body.innerHTML=document.body.innerHTML.replace(/\t\t/g,'</br>').replace(/StatTrakâ„¢/g,'StatTrak™').replace(/&lt;marketname&gt;/g,'<span class="hover_bold">').replace(/&lt;\/marketname&gt;&lt;price&gt;/g,'\t-\t$').replace(/&lt;\/price&gt;/g,'').replace(/<\/br>/g,"</span></br>");
